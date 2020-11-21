9a995c3b61a5c5068f3bb413dd86166095522ede
Michael Stephens <mdstephens26@yahoo.com>
2020-11-21 10:19:07 -0500

    commit tree 2a6ccf28e042e04b2e76cd426200666e895a5f1f

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
-def square_array(array)
-  # your code here
+require 'pry'
+def square_array(numbers)
+  new_array = []
+  numbers.each do |numbers| 
+  new_array << numbers ** 2
+end
+return new_array
 end
